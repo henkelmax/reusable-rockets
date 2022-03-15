bb7dbfe31de75941cb2c7d45fb7551956dbcb56b
Max Henkel <max@maxhenkel.de>
2022-03-15 16:13:20 +0100

    Re add JEI integration

--- src/main/java/de/maxhenkel/rockets/integration/jei/RefuelExtension.java
+++ src/main/java/de/maxhenkel/rockets/integration/jei/RefuelExtension.java
@@ -3,17 +3,16 @@ package de.maxhenkel.rockets.integration.jei;
 import de.maxhenkel.rockets.item.ItemReusableRocket;
 import de.maxhenkel.rockets.recipe.RefuelRecipe;
 import mezz.jei.api.constants.VanillaTypes;
-import mezz.jei.api.gui.IRecipeLayout;
-import mezz.jei.api.gui.ingredient.IGuiItemStackGroup;
-import mezz.jei.api.ingredients.IIngredients;
-import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICustomCraftingCategoryExtension;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.ingredient.ICraftingGridHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.category.extensions.vanilla.crafting.ICraftingCategoryExtension;
 import net.minecraft.world.item.ItemStack;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class RefuelExtension<T extends RefuelRecipe> implements ICustomCraftingCategoryExtension {
+public class RefuelExtension<T extends RefuelRecipe> implements ICraftingCategoryExtension {
 
     private final T recipe;
 
@@ -22,40 +21,23 @@ public class RefuelExtension<T extends RefuelRecipe> implements ICustomCraftingC
     }
 
     @Override
-    public void setRecipe(IRecipeLayout layout, IIngredients ingredients) {
-        IGuiItemStackGroup guiItemStacks = layout.getItemStacks();
-
+    public void setRecipe(IRecipeLayoutBuilder builder, ICraftingGridHelper craftingGridHelper, IFocusGroup focuses) {
         ItemStack in = recipe.getRocket().copy();
-        if (!(in.getItem() instanceof ItemReusableRocket)) {
+        if (!(in.getItem() instanceof ItemReusableRocket rocket)) {
             return;
         }
-        ItemReusableRocket rocket = (ItemReusableRocket) in.getItem();
         rocket.setUsesLeft(in, rocket.getMaxUses() / 2);
 
-        ItemStack out = in.copy();
-        rocket.setUsesLeft(out, Math.min(rocket.getUsesLeft(in) + 8, rocket.getMaxUses()));
+        ItemStack out1 = in.copy();
+        rocket.setUsesLeft(out1, Math.min(rocket.getUsesLeft(in) + 8, rocket.getMaxUses()));
 
         List<ItemStack> fuels = Arrays.asList(recipe.getFuel().getItems());
-        List<ItemStack> fuels2 = Arrays.asList(recipe.getFuel().getItems());
-
-        guiItemStacks.set(0, out);
-        guiItemStacks.set(1, fuels);
-        guiItemStacks.set(2, fuels2);
-        guiItemStacks.set(3, fuels);
-        guiItemStacks.set(4, fuels);
-        guiItemStacks.set(5, in);
-        guiItemStacks.set(6, fuels);
-        guiItemStacks.set(7, fuels);
-        guiItemStacks.set(8, fuels);
-        guiItemStacks.set(9, fuels);
+        craftingGridHelper.setInputs(builder, VanillaTypes.ITEM, Arrays.asList(
+                fuels, fuels, fuels,
+                fuels, List.of(in), fuels,
+                fuels, fuels, fuels
+        ), 0, 0);
+        craftingGridHelper.setOutputs(builder, VanillaTypes.ITEM, List.of(out1));
     }
 
-    @Override
-    public void setIngredients(IIngredients ingredients) {
-        List<ItemStack> inputs = new ArrayList<>(Arrays.asList(recipe.getFuel().getItems()));
-        inputs.add(recipe.getRocket());
-
-        ingredients.setInputs(VanillaTypes.ITEM, inputs);
-        ingredients.setOutput(VanillaTypes.ITEM, recipe.getRocket());
-    }
 }
