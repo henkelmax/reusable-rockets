8a7ac43bd59568438909f5808dcad67c75e0d33b
Max Henkel <max@maxhenkel.de>
2023-11-19 22:38:50 +0100

    Update neoforge

--- src/main/java/de/maxhenkel/rockets/Main.java
+++ src/main/java/de/maxhenkel/rockets/Main.java
@@ -4,6 +4,7 @@ import de.maxhenkel.corelib.CommonRegistry;
 import de.maxhenkel.rockets.events.CreativeTabEvents;
 import de.maxhenkel.rockets.item.ItemReusableRocket;
 import de.maxhenkel.rockets.recipe.RefuelRecipe;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
@@ -12,9 +13,8 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.ForgeRegistries;
-import net.neoforged.neoforge.registries.RegistryObject;
 
 @Mod(Main.MODID)
 public class Main {
@@ -23,19 +23,19 @@ public class Main {
 
     public static ServerConfig SERVER_CONFIG;
 
-    private static final DeferredRegister<RecipeSerializer<?>> RECIPE_REGISTER = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, Main.MODID);
+    private static final DeferredRegister<RecipeSerializer<?>> RECIPE_REGISTER = DeferredRegister.create(BuiltInRegistries.RECIPE_SERIALIZER, Main.MODID);
 
-    public static final RegistryObject<RecipeSerializer<?>> CRAFTING_REFUEL = RECIPE_REGISTER.register("refuel", RefuelRecipe.RecipeRefuelSerializer::new);
+    public static final DeferredHolder<RecipeSerializer<?>, RecipeSerializer<?>> CRAFTING_REFUEL = RECIPE_REGISTER.register("refuel", RefuelRecipe.RecipeRefuelSerializer::new);
 
-    private static final DeferredRegister<Item> ITEM_REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MODID);
+    private static final DeferredRegister<Item> ITEM_REGISTER = DeferredRegister.create(BuiltInRegistries.ITEM, Main.MODID);
 
-    public static final RegistryObject<ItemReusableRocket> REUSABLE_ROCKET_TIER_1 = ITEM_REGISTER.register("reusable_rocket_tier_1", () ->
+    public static final DeferredHolder<Item, ItemReusableRocket> REUSABLE_ROCKET_TIER_1 = ITEM_REGISTER.register("reusable_rocket_tier_1", () ->
             new ItemReusableRocket(SERVER_CONFIG.tier1MaxUses::get, SERVER_CONFIG.tier1MaxDuration::get)
     );
-    public static final RegistryObject<ItemReusableRocket> REUSABLE_ROCKET_TIER_2 = ITEM_REGISTER.register("reusable_rocket_tier_2", () ->
+    public static final DeferredHolder<Item, ItemReusableRocket> REUSABLE_ROCKET_TIER_2 = ITEM_REGISTER.register("reusable_rocket_tier_2", () ->
             new ItemReusableRocket(SERVER_CONFIG.tier2MaxUses::get, SERVER_CONFIG.tier2MaxDuration::get)
     );
-    public static final RegistryObject<ItemReusableRocket> REUSABLE_ROCKET_TIER_3 = ITEM_REGISTER.register("reusable_rocket_tier_3", () ->
+    public static final DeferredHolder<Item, ItemReusableRocket> REUSABLE_ROCKET_TIER_3 = ITEM_REGISTER.register("reusable_rocket_tier_3", () ->
             new ItemReusableRocket(SERVER_CONFIG.tier3MaxUses::get, SERVER_CONFIG.tier3MaxDuration::get)
     );
 
